1abbc2652601e44197651e966d796bb093a5cf69
Robbie op de Weegh <robbie@nocks.com>
2019-08-17 11:53:28 +0200

    TradeMarket transform "base_currency" and "quote_currency"

--- src/api/transformers/TradeMarket.js
+++ src/api/transformers/TradeMarket.js
@@ -5,14 +5,37 @@ const AmountTransformer = require('./Amount');
  *
  * @param tradeMarket
  */
-const transform = (tradeMarket) => AmountTransformer.transform(Object.assign({}, tradeMarket));
+const transform = (tradeMarket) => AmountTransformer.transform(Object.assign({}, tradeMarket, {
+  base_currency: tradeMarket.base_currency && tradeMarket.base_currency.data
+    ? AmountTransformer.transform(tradeMarket.base_currency.data) : undefined,
+
+  quote_currency: tradeMarket.quote_currency && tradeMarket.quote_currency.data
+    ? AmountTransformer.transform(tradeMarket.quote_currency.data) : undefined,
+}));
 
 /**
  * Reverse transform tradeMarket
  *
  * @param tradeMarket
+ * @param prepareForRequest
  */
-const reverseTransform = (tradeMarket) => AmountTransformer.reverseTransform(Object.assign({}, tradeMarket));
+const reverseTransform = (tradeMarket, { prepareForRequest = false } = {}) => {
+  const reverseObject = {};
+
+  if (tradeMarket.base_currency && !prepareForRequest) {
+    reverseObject.base_currency = {
+      data: AmountTransformer.reverseTransform(tradeMarket.base_currency),
+    };
+  }
+
+  if (tradeMarket.quote_currency && !prepareForRequest) {
+    reverseObject.quote_currency = {
+      data: AmountTransformer.reverseTransform(tradeMarket.quote_currency),
+    };
+  }
+
+  return AmountTransformer.reverseTransform(Object.assign({}, tradeMarket, reverseObject));
+};
 
 module.exports = {
   transform,
